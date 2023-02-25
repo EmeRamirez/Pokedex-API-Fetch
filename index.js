d3d137d2412c94df7d01a2f0d9c15abd12aeac58
Emerson Ramirez <emerson.ramirez.s@gmail.com>
2023-02-25 01:26:09 -0300

    nvo boton

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { getPkmn } from "./class/funciones.js";
 
 
 const btnBusqueda = document.querySelector("#btn-busqueda");
+const btnBusqueda2 = document.querySelector("#btn-busqueda2");
+
 
 btnBusqueda.addEventListener("click",()=>{
     const inputNum = document.querySelector("#input-num");
@@ -15,6 +17,24 @@ btnBusqueda.addEventListener("click",()=>{
         .then(pkmn => {
             const img = document.querySelector(".imagen-dex > img");
             img.src=pkmn.sprites.front_default;
+            // img.src=pkmn.sprites.other["official-artwork"].front_default
+        })
+    }
+})
+
+btnBusqueda2.addEventListener("click",()=>{
+    const inputNum = document.querySelector("#input-num");
+    let num = parseInt(inputNum.value);
+
+    if (num < 1 || num > 1008){
+        const img = document.querySelector(".imagen-dex > img");
+        img.src="img/sukamon1.png"
+    } else {
+        getPkmn(num)
+        .then(pkmn => {
+            const img = document.querySelector(".imagen-dex > img");
+            // img.src=pkmn.sprites.front_default;
+            img.src=pkmn.sprites.other["official-artwork"].front_default
         })
     }
 })
